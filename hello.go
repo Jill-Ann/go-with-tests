6ce14a2212a7f100aad523f9315f1b976def06ea
Jill-Ann <jillannkay@gmail.com>
2021-05-01 12:39:39 +0200

    Create constants for languages

--- hello.go
+++ hello.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const portuguese = "Portuguese"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const portugueseHelloPrefix = "Ola, "
 
 func Hello(name string) string {
 	if name == "" {
