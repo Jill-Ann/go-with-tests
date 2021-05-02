54a5c4754f4c6956316aac3cec61631016b2f762
Jill-Ann <jillannkay@gmail.com>
2021-05-02 14:35:02 +0200

    Refactor test

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,10 +1,13 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("c")
-	expected := ("ccccc")
+	repeated := Repeat("c", 3)
+	expected := ("ccc")
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
@@ -13,6 +16,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 0)
 	}
 }
