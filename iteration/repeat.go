5da4663a4c4bbb2801fd01049c240d21a8c4d2b1
Jill-Ann <jillannkay@gmail.com>
2021-05-02 14:34:39 +0200

    Refactor repeat function

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,11 +1,9 @@
 package iteration
 
-const repeatCount = 5
-
-func Repeat(char string) string {
+func Repeat(char string, count int) string {
 	var repeated string
 
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < count; i++ {
 		repeated += char
 	}
 
